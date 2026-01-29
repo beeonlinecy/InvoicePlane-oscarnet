63c2a7f3516bf8e4ee5cd19f6785031e7d6a8f4e
Alex Home-PC <beeonline.cy@gmail.com>
2026-01-29 22:21:51 +0200

    Added bootstrap lib

--- manager/application/views/auth/login.php
+++ manager/application/views/auth/login.php
@@ -1,18 +1,27 @@
-<!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>Login</title>
+<link rel="stylesheet" href="/manager/public/assets/bootstrap/css/bootstrap.min.css">
+<script src="/manager/public/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
-
-<h2>Login</h2>
-
-<form method="post" action="login/auth">
-    <input type="email" name="email" required>
-    <input type="password" name="password" required>
-    <button type="submit">Login</button>
-</form>
-
+<div class="container my-5">
+    <h2 class="text-center mb-4">Login</h2>
+    
+    <form method="post" action="login/auth" class="mx-auto w-25 bg-light p-3 rounded">
+        <div class="mb-3">
+            <label for="email" class="form-label">Email address</label>
+            <input type="email" name="email" required class="form-control" id="email">
+        </div>
+        
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" name="password" required class="form-control" id="password">
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+</div>
 </body>
 </html>
+
